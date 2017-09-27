65d64068ddf7e7dbd614291a7e466b9dd25cc20d
Ramon Chiara <ramonchiara@gmail.com>
2017-09-27 18:16:57 -0300

    Lembrar que deve gerar JSON.

--- src/java/br/pro/ramon/dcs/jaxrs/pessoa/PessoaResource.java
+++ src/java/br/pro/ramon/dcs/jaxrs/pessoa/PessoaResource.java
@@ -3,7 +3,9 @@ package br.pro.ramon.dcs.jaxrs.pessoa;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/pessoas")
@@ -11,6 +13,7 @@ public class PessoaResource {
 
     @GET
     @Path("/{nome}/imc")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getImc(@PathParam("nome") String nome, @QueryParam("peso") double peso, @QueryParam("altura") double altura) {
         Pessoa pessoa = new Pessoa(nome, peso, altura);
         Imc imc = pessoa.getImc();
